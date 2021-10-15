3f0870b9e3aeb91ee756cbf5c695dc99789c54dd
pedroribeirophlr@gmail.com <pedroribeirophlr@gmail.com>
2021-10-15 12:27:43 +0000

    criação de dim e facts

---
+++ models/northwind/mart/dim_customers.sql
@@ -0,0 +1,27 @@
+{{ config(materialized='table') }}
+
+with
+    staging as (
+        select *
+        from {{ ref('stg_customer') }}
+)
+    , transformed as (
+        select
+
+
+           row_number() over (order by customer_id) as customer_sk -- auto incremental surrogate key
+           ,customer_id		
+           ,company_name		
+           ,contact_name		
+           ,contact_title	
+           ,address		
+           ,city		
+           ,region		
+           ,postal_code	
+           ,country		
+           ,phone		
+           ,fax
+        from staging     
+)
+
+select * from transformed
--- models/northwind/mart/fact_order.sql
+++ models/northwind/mart/fact_order.sql
@@ -1,4 +1,29 @@
 {{ config(materialized='table') }}
 
-select * 
-from {{ ref('stg_orders') }}
+with 
+    customers as (
+        select
+        customer_sk
+        ,customer_id
+        from {{ref('dim_customers')}}
+    )
+
+, orders_with_sk as (
+        select
+        orders.order_id
+        , customers.customer_id
+        , orders.order_date
+        , orders.ship_region
+        , orders.shipped_date
+        , orders.ship_country
+        , orders.ship_address
+        , orders.ship_postal_code
+        , orders.ship_city
+        , orders.ship_name
+        , orders.freight
+        , orders.required_date 
+
+    from {{ref('stg_orders')}} orders
+    left join customers customers on orders.customer_id = customers.customer_id
+)
+select * from orders_with_sk
